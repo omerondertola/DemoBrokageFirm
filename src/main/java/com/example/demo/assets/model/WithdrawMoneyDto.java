a45d1f9fe78e1c8abe02ba64e62f4063996c4654
Ömer Önder Tola <omerondertola@gmail.com>
2024-10-28 09:54:19 +0300

    validation

--- src/main/java/com/example/demo/assets/model/WithdrawMoneyDto.java
+++ src/main/java/com/example/demo/assets/model/WithdrawMoneyDto.java
@@ -3,7 +3,7 @@ package com.example.demo.assets.model;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
 public record WithdrawMoneyDto(
 
@@ -12,7 +12,7 @@ public record WithdrawMoneyDto(
 
         @NotNull(message = "iban shall exists")
         @NotEmpty(message = "iban shall not be empty-string")
-        @Length(message = "length shall be 26", min = 26, max = 26)
+        @Size(message = "length shall be 26", min = 26, max = 26)
         String iban
 ) {
 }
