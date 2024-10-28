33fc4e9e59f4aae4ae5bc626f86e39d5006adcf2
Ömer Önder Tola <omerondertola@gmail.com>
2024-10-28 10:19:28 +0300

    documented.

--- src/main/java/com/example/demo/configuration/ObserverConfig.java
+++ src/main/java/com/example/demo/configuration/ObserverConfig.java
@@ -5,13 +5,27 @@ import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Marks this class as a configuration class for Spring, meaning it contains bean definitions.
+ * "proxyBeanMethods = false" improves performance by disabling proxying of @Bean methods.
+ * Configuration class for setting up observation (monitoring) aspects in the application.
+ */
 @Configuration(proxyBeanMethods = false)
 public class ObserverConfig {
 
-    // To have the @Observed support we need to register this aspect
+    /**
+     * Registers an ObservedAspect bean, which enables the @Observed annotation support.
+     * The @Observed annotation allows for automatic instrumentation of methods for observation.
+     * The ObservationRegistry is required by ObservedAspect to manage observation events.
+     *
+     * @param observationRegistry
+     * @return
+     */
     @Bean
     ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        // Defines a Spring bean of type ObservedAspect, using the ObservationRegistry as a dependency.
+        // This bean enables Micrometer’s @Observed annotation, allowing methods annotated with @Observed
+        // to be automatically instrumented for monitoring, capturing timing and other metrics.
         return new ObservedAspect(observationRegistry);
     }
-
 }
